d7a1248fe676135bddc867bad8507ee7ab0c213b
Mambouna <mambouna@posteo.net>
2025-09-23 09:59:19 +0200

    Changed pgzrun occurences in pgtrun.py

--- src/pgtrun.py
+++ src/pgtrun.py
@@ -1,15 +1,15 @@
-"""Runner system for Pygame Zero.
+"""Runner system for Pygame Turbo.
 
 By importing this module, the __main__ module is populated with the builtins
-provided by Pygame Zero.
+provided by Pygame Turbo.
 
-When pgtrun.go() is called, the __main__ module is run as a Pygame Zero
+When pgtrun.go() is called, the __main__ module is run as a Pygame Turbo
 script (we enter the game loop, calling draw() and update() etc as defined in
 __main__).
 
 """
 import sys
-from pgzero.runner import prepare_mod, run_mod
+from pgturbo.runner import prepare_mod, run_mod
 
 
 mod = sys.modules['__main__']
@@ -17,13 +17,13 @@ if not getattr(sys, '_pgtrun', None):
     if not getattr(mod, '__file__', None):
         raise ImportError(
             "You are running from an interactive interpreter.\n"
-            "'import pgzrun' only works when you are running a Python file."
+            "'import pgtrun' only works when you are running a Python file."
         )
     prepare_mod(mod)
 
 
 def go():
-    """Run the __main__ module as a Pygame Zero script."""
+    """Run the __main__ module as a Pygame Turbo script."""
     if getattr(sys, '_pgtrun', None):
         return
 
