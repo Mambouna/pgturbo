6eb7a3a39c1a129efcaac4663cceeb1e4d1bba54
Mambouna <mambouna@posteo.net>
2025-10-22 14:38:53 +0200

    Typo fix

--- src/pgturbo/mouse.py
+++ src/pgturbo/mouse.py
@@ -228,7 +228,7 @@ class Mouse:
             self._cursor_image_name = None
             try:
                 pygame.mouse.set_cursor(system_cursors[c_string])
-            except pygame.Error:
+            except pygame.error:
                 pygame.mouse.set_cursor()
                 print("WARNING: Cursor could not be set. Not all platforms "
                       "support all system cursors. Consider using a custom "
