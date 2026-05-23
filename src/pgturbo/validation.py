151b56f17e8fb0b4c46258621c1bd7772a8ac692
Mambouna <mambouna@posteo.net>
2026-05-23 15:52:35 +0200

    Fixed flake issue

--- src/pgturbo/validation.py
+++ src/pgturbo/validation.py
@@ -1,6 +1,7 @@
 def validate_position_value(value):
-    error_msg=("Positions must be tuples or lists with two integer or float "
-               "values for the X and Y coordinates, e.g. (10, 25). You used: ")
+    error_msg = ("Positions must be tuples or lists with two integer or float "
+                 "values for the X and Y coordinates, e.g. (10, 25). You "
+                 "used: ")
     try:
         x, y = value
     except TypeError:
